d18d5752f8553e54d69a2a0d1516d95bcfeec9d1
gofrendi <gofrendiasgard@gmail.com>
2019-12-21 21:30:34 +0700

    add DoAction

--- action/do.go
+++ action/do.go
@@ -4,13 +4,38 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/state-alchemists/zaruba/command"
 	"github.com/state-alchemists/zaruba/dir"
 )
 
+// DoActionOption is option for action.Do
+type DoActionOption struct {
+	MTime       time.Time
+	PerformPre  bool
+	PerformPost bool
+}
+
+// DefaultDoOption is default option used for Do
+var DefaultDoOption DoActionOption = DoActionOption{
+	MTime:       time.Time{},
+	PerformPre:  true,
+	PerformPost: true,
+}
+
 // Do action on projectDir
 func Do(actionString, projectDir string, arguments ...string) (err error) {
+	return DoAction(
+		actionString,
+		projectDir,
+		DefaultDoOption,
+		arguments...,
+	)
+}
+
+// DoAction action on projectDir
+func DoAction(actionString, projectDir string, option DoActionOption, arguments ...string) (err error) {
 	projectDir, err = filepath.Abs(projectDir)
 	if err != nil {
 		return
@@ -22,24 +47,28 @@ func Do(actionString, projectDir string, arguments ...string) (err error) {
 		return
 	}
 	// pre-action
-	if err = processAllDirs("pre-"+actionString, allDirs, arguments...); err != nil {
-		return
+	if option.PerformPre {
+		if err = processAllDirs("pre-"+actionString, allDirs, option, arguments...); err != nil {
+			return
+		}
 	}
 	// action
-	if err = processAllDirs(actionString, allDirs, arguments...); err != nil {
+	if err = processAllDirs(actionString, allDirs, option, arguments...); err != nil {
 		return
 	}
 	// post-action
-	err = processAllDirs("post-"+actionString, allDirs, arguments...)
+	if option.PerformPost {
+		err = processAllDirs("post-"+actionString, allDirs, option, arguments...)
+	}
 	return
 }
 
-func processAllDirs(actionString string, allDirs []string, arguments ...string) (err error) {
+func processAllDirs(actionString string, allDirs []string, option DoActionOption, arguments ...string) (err error) {
 	// start multiple processDir as go-routines
 	errChans := []chan error{}
 	for _, dir := range allDirs {
 		errChan := make(chan error)
-		go processDir(errChan, actionString, dir, arguments...)
+		go processDir(errChan, actionString, dir, option, arguments...)
 		errChans = append(errChans, errChan)
 	}
 	// wait all go-routine finished
@@ -52,7 +81,7 @@ func processAllDirs(actionString string, allDirs []string, arguments ...string) 
 	return
 }
 
-func processDir(errChan chan error, actionString, dir string, arguments ...string) {
+func processDir(errChan chan error, actionString, dir string, option DoActionOption, arguments ...string) {
 	actionPath := filepath.Join(dir, fmt.Sprintf("./%s", actionString))
 	if _, err := os.Stat(actionPath); err != nil {
 		// if file is not exists
