8d16780d8e79cfe6eda68812660ff49ea85bb9a3
gofrendi <gofrendiasgard@gmail.com>
2020-04-05 13:09:16 +0700

    Zaruba: syncing `nodejs-service-template` at 2020-04-05T13:09:15+07:00

--- templates/nodejs-service/service/src/bootstrap/setup.ts
+++
@@ -1,8 +0,0 @@
-import { Setting } from "../components/setting";
-import { setup as monitoringSetup } from "../components/monitoring";
-import { setup as exampleSetup } from "../components/example";
-
-export function setup(s: Setting) {
-    monitoringSetup(s);
-    exampleSetup(s);
-}
--- templates/nodejs-service/service/src/components/example/rpcHandler.ts
+++
@@ -1,9 +0,0 @@
-import { greet } from "./serviceGreeting";
-
-export function greetRpcController(...inputs: any[]): any {
-    if (inputs.length === 0) {
-        throw new Error("Message accepted but input is invalid");
-    }
-    const name = inputs[0] as string
-    return greet(name);
-}
