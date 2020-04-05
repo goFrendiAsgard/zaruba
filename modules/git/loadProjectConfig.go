6336193dd49ea9a5414aaf527d6424ccdcc72998
gofrendi <gofrendiasgard@gmail.com>
2020-04-05 13:33:18 +0700

    Zaruba: Save before push to sub-repos at 2020-04-05T13:33:18+07:00

--- modules/git/loadProjectConfig.go
+++ modules/git/loadProjectConfig.go
@@ -24,21 +24,15 @@ func LoadProjectConfig(projectDir string) (p *config.ProjectConfig, currentBranc
 		return p, currentBranchName, currentGitRemotes, err
 	}
 	// check all component's origin
+	errChans := []chan error{}
 	for componentName, component := range p.GetComponents() {
-		origin := component.GetOrigin()
-		if origin == "" {
-			continue
-		}
-		logger.Info("Check origin of component `%s`", componentName)
-		output, err := LsRemote(projectDir, origin)
-		if err != nil {
+		errChan := make(chan error)
+		errChans = append(errChans, errChan)
+		go checkComponent(projectDir, componentName, component, errChan)
+	}
+	for _, errChan := range errChans {
+		if err = <-errChan; err != nil {
 			return p, currentBranchName, currentGitRemotes, err
-		}
-		// an empty repo doesn't output anything.
-		if output == "" {
-			if err = initRepo(projectDir, componentName, component); err != nil {
-				return p, currentBranchName, currentGitRemotes, err
-			}
 		}
 	}
 	// get currentBranchName
@@ -49,6 +43,29 @@ func LoadProjectConfig(projectDir string) (p *config.ProjectConfig, currentBranc
 	// get currentGitRemotes
 	currentGitRemotes, err = GetCurrentGitRemotes(projectDir)
 	return p, currentBranchName, currentGitRemotes, err
+}
+
+func checkComponent(projectDir string, componentName string, component *config.Component, errChan chan error) {
+	origin := component.GetOrigin()
+	if origin == "" {
+		errChan <- nil
+		return
+	}
+	logger.Info("Check origin of component `%s`", componentName)
+	output, err := LsRemote(projectDir, origin)
+	if err != nil {
+		errChan <- err
+		return
+	}
+	// an empty repo doesn't output anything.
+	if output == "" {
+		if err = initRepo(projectDir, componentName, component); err != nil {
+			errChan <- err
+			return
+		}
+	}
+	errChan <- nil
+	return
 }
 
 func initRepo(projectDir, componentName string, component *config.Component) (err error) {
