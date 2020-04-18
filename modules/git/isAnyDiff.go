f9270c2fff26120f3462d43ab08d0d380d6449fb
gofrendi <gofrendiasgard@gmail.com>
2020-04-18 16:24:41 +0700

    Zaruba: Save before push to sub-repos at 2020-04-18T16:24:41+07:00

--- modules/git/isAnyDiff.go
+++ modules/git/isAnyDiff.go
@@ -10,10 +10,10 @@ import (
 // IsAnyDiff get whether there is diff or not
 func IsAnyDiff(projectDir string) bool {
 	logger.Info("Get current git branch")
-	output, err := command.Run(projectDir, "git", "diff", "HEAD", "--exit-code")
+	output, err := command.Run(projectDir, "git", "--no-pager", "diff", "HEAD")
 	if err != nil {
-		return true
+		return false
 	}
 	diff := strings.Trim(output, "\r\n ")
-	return diff == ""
+	return diff != ""
 }
