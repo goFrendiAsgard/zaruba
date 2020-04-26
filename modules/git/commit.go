3bc99d52645c6333ed1a85a24bd8a7d8e7c94cd6
gofrendi <gofrendiasgard@gmail.com>
2020-04-26 16:41:46 +0700

    add commitIfAnyDiff

--- modules/git/commit.go
+++ modules/git/commit.go
@@ -5,12 +5,17 @@ import (
 	"github.com/state-alchemists/zaruba/modules/logger"
 )
 
-// Commit add and commit
-func Commit(projectDir, message string) {
+// CommitIfAnyDiff add and commit
+func CommitIfAnyDiff(projectDir, message string) {
 	if !IsAnyDiff(projectDir) {
 		logger.Info("Nothing to commit")
 		return
 	}
+	Commit(projectDir, message)
+}
+
+// Commit add and commit
+func Commit(projectDir, message string) {
 	logger.Info("Git add and git commit with messsage `%s`", message)
 	if err := command.RunAndRedirect(projectDir, "git", "add", ".", "-A"); err != nil {
 		logger.Error("%s", err)
