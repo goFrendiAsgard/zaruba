510dc204518694fbc0b5d5e0dcf61962406d4853
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2020-11-11 07:12:35 +0700

    add line number

--- runner/runner.go
+++ runner/runner.go
@@ -403,6 +403,9 @@ func (r *Runner) sprintfCmdArgs(cmd *exec.Cmd) (output string) {
 			if index == 0 {
 				prefix = "* "
 			}
+			if len(rows) > 1 {
+				prefix += fmt.Sprintf("%s%4d |%s ", d.Yellow, index+1, d.NoColor)
+			}
 			row = fmt.Sprintf("%s   %s%s%s%s", r.Spaces, d.Faint, prefix, row, d.Normal)
 			rows[index] = row
 		}
