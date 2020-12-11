a9765b0e01b3b3f876671e7d429c19166b4b4d31
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2020-12-11 17:14:28 +0700

    use pipenv sync

--- scripts/util/sh/py_install.sh
+++ scripts/util/sh/py_install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # USAGE
-# sh py.sh <py-util> [arg1]... [arg9]
+# sh py_install.sh <py-util> [arg1]... [arg9]
 
-_CURRENT_DIR=$(pwd)
 _SH_UTIL=$(dirname $(realpath $0))
 _PY_UTIL=$(realpath "${_SH_UTIL}/../python")
 
 export PIPENV_DONT_LOAD_ENV=1
 export PIPENV_PIPFILE="${_PY_UTIL}/Pipfile"
-pipenv install
+pipenv sync
