354d938996cc573c9622b2522f992b27f5147e83
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2020-12-11 10:55:51 +0700

    add py_install script

--- scripts/util/sh/py_install.sh
+++ scripts/util/sh/py_install.sh
@@ -3,9 +3,10 @@
 # USAGE
 # sh py.sh <py-util> [arg1]... [arg9]
 
+_CURRENT_DIR=$(pwd)
 _SH_UTIL=$(dirname $(realpath $0))
 _PY_UTIL=$(realpath "${_SH_UTIL}/../python")
 
 export PIPENV_DONT_LOAD_ENV=1
 export PIPENV_PIPFILE="${_PY_UTIL}/Pipfile"
-pipenv run python "${_PY_UTIL}/${1}.py" "${2}" "${3}" "${4}" "${5}" "${6}" "${7}" "${8}" "${9}" "${10}"
+pipenv install
