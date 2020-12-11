cebc7122748940eb1c9229b85db5742383974f09
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2020-12-11 10:46:09 +0700

    remove pipenv install

--- scripts/util/sh/py.sh
+++ scripts/util/sh/py.sh
@@ -9,5 +9,4 @@ _PY_UTIL=$(realpath "${_SH_UTIL}/../python")
 
 export PIPENV_DONT_LOAD_ENV=1
 export PIPENV_PIPFILE="${_PY_UTIL}/Pipfile"
-pipenv install &> /dev/null
 pipenv run python "${_PY_UTIL}/${1}.py" "${2}" "${3}" "${4}" "${5}" "${6}" "${7}" "${8}" "${9}" "${10}"
