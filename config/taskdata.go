282576e423ecf9c2e62816abf88950b78d8d954a
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-03-18 08:47:59 +0700

    add ReplaceAllWith

--- config/taskdata.go
+++ config/taskdata.go
@@ -218,3 +218,17 @@ func (td *TaskData) ParseFile(filePath string) (parsedStr string, err error) {
 	}
 	return b.String(), nil
 }
+
+// ReplaceAllWith
+func (td *TaskData) ReplaceAllWith(s string, replacements ...string) (result string) {
+	if len(replacements) < 2 {
+		return s
+	}
+	result = s
+	new := replacements[len(replacements)-1]
+	olds := replacements[:len(replacements)-1]
+	for _, old := range olds {
+		result = strings.ReplaceAll(result, old, new)
+	}
+	return result
+}
