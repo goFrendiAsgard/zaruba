3394cc36cfecf58cddcf648cf945998e0bf1a976
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-03-29 09:05:02 +0700

    fix get_segment

--- scripts/util/python/get_segment.py
+++ scripts/util/python/get_segment.py
@@ -2,8 +2,9 @@ from helper import cli
 
 
 @cli
-def show_segment(text: str, separator: str, index: int) -> str:
-    print(text.split(separator)[index])
+def show_segment(text: str, separator: str, index: str) -> str:
+    list_index=int(index)
+    print(text.split(separator)[list_index])
 
 
 if __name__ == '__main__':
