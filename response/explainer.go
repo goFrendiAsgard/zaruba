bc41b18e857cfca4f2ac6a43bbbdaa41e7201e7c
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-04-20 08:04:40 +0700

    better explainer

--- response/explainer.go
+++ response/explainer.go
@@ -22,15 +22,6 @@ func NewExplainer(logger monitor.Logger, decoration *monitor.Decoration, project
 	}
 }
 
-func (e *Explainer) printField(fieldName string, value string, indentation string) {
-	if value != "" {
-		trimmedValue := strings.Trim(value, "\n ")
-		valueLines := strings.Split(trimmedValue, "\n")
-		indentedValue := strings.Join(valueLines, "\n"+indentation)
-		e.logger.DPrintf("%s%s :%s %s\n", e.d.Yellow, fieldName, e.d.Normal, indentedValue)
-	}
-}
-
 func (e *Explainer) Explain(taskName string) {
 	task := e.project.Tasks[taskName]
 	indentation := strings.Repeat(" ", 21)
@@ -38,32 +29,60 @@ func (e *Explainer) Explain(taskName string) {
 	if len(parentTasks) == 0 && task.Extend != "" {
 		parentTasks = []string{task.Extend}
 	}
-	inputString := ""
+	e.printField("TASK NAME   ", taskName, indentation)
+	e.printField("LOCATION    ", task.GetFileLocation(), indentation)
+	e.printField("DESCRIPTION ", task.Description, indentation)
+	e.printField("INPUTS      ", e.getInputString(task), indentation)
+	e.printField("DEPENDENCIES", strings.Join(task.Dependencies, ", "), indentation)
+	e.printField("PARENT TASKS", strings.Join(parentTasks, ", "), indentation)
+}
+
+func (e *Explainer) getInputString(task *config.Task) (inputString string) {
 	inputNames := task.Inputs
 	inputCount := len(inputNames)
 	if inputCount > 0 {
 		paramIndentation := strings.Repeat(" ", 2)
-		paramFieldIndentation := paramIndentation + strings.Repeat(" ", 12)
 		for _, inputName := range inputNames {
 			input := e.project.Inputs[inputName]
-			inputOptionStr := strings.Join(input.Options, ", ")
-			inputDesc := strings.Trim(input.Description, "\n")
-			inputDescLines := strings.Split(inputDesc, "\n")
-			inputDescStr := strings.Join(inputDescLines, "\n  "+paramFieldIndentation)
-			inputLines := []string{
+			rawInputLines := []string{
 				fmt.Sprintf("- %s", inputName),
-				fmt.Sprintf("%s%sOPTIONS     :%s %s", e.d.Yellow, paramIndentation, e.d.Normal, inputOptionStr),
-				fmt.Sprintf("%s%sDESCRIPTION :%s %s", e.d.Yellow, paramIndentation, e.d.Normal, inputDescStr),
+				e.getInputFieldString("DESCRIPTION", input.Description, paramIndentation),
+				e.getInputFieldString("PROMPT     ", input.Prompt, paramIndentation),
+				e.getInputFieldString("OPTIONS    ", strings.Join(input.Options, ", "), paramIndentation),
+				e.getInputFieldString("DEFAULT    ", input.DefaultValue, paramIndentation),
+				e.getInputFieldString("VALIDATION ", input.Validation, paramIndentation),
+			}
+			inputLines := []string{}
+			for _, inputLine := range rawInputLines {
+				if inputLine != "" {
+					inputLines = append(inputLines, inputLine)
+				}
 			}
 			inputString += "\n" + strings.Trim(strings.Join(inputLines, "\n"), "\n")
 		}
 	}
-	e.printField("TASK NAME   ", taskName, indentation)
-	e.printField("LOCATION    ", task.GetFileLocation(), indentation)
-	e.printField("DESCRIPTION ", task.Description, indentation)
-	e.printField("INPUTS      ", inputString, indentation)
-	e.printField("DEPENDENCIES", strings.Join(task.Dependencies, ", "), indentation)
-	e.printField("PARENT TASKS", strings.Join(parentTasks, ", "), indentation)
+	return inputString
+}
+
+func (e *Explainer) getInputFieldString(inputFieldName string, inputFieldValue string, paramIndentation string) (inputFieldString string) {
+	inputFieldValue = strings.Trim(inputFieldValue, "\n")
+	if inputFieldValue == "" {
+		return ""
+	}
+	paramFieldIndentation := paramIndentation + strings.Repeat(" ", 12)
+	inputFieldLines := strings.Split(strings.Trim(inputFieldValue, "\n"), "\n")
+	inputFieldValueStr := strings.Join(inputFieldLines, "\n  "+paramFieldIndentation)
+	return fmt.Sprintf("%s%s%s :%s %s", paramIndentation, e.d.Yellow, inputFieldName, e.d.Normal, inputFieldValueStr)
+}
+
+func (e *Explainer) printField(fieldName string, value string, indentation string) {
+	if value == "" {
+		return
+	}
+	trimmedValue := strings.Trim(value, "\n ")
+	valueLines := strings.Split(trimmedValue, "\n")
+	indentedValue := strings.Join(valueLines, "\n"+indentation)
+	e.logger.DPrintf("%s%s :%s %s\n", e.d.Yellow, fieldName, e.d.Normal, indentedValue)
 }
 
 func (e *Explainer) GetCommand(taskNames []string) (command string) {
