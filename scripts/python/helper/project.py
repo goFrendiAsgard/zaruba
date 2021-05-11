7e807a2073fa25ebcd036d9396178ddd1f0e80df
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-05-11 15:21:16 +0700

    fix split

--- scripts/python/helper/project.py
+++ scripts/python/helper/project.py
@@ -200,7 +200,7 @@ class TaskProject(Project):
         env_dict: Mapping[str, str] = {}
         for env_pair in env_list:
             env_pair_parts = env_pair.split('=')
-            env_key, env_val = env_pair[0], '='.join(env_pair[1:])
+            env_key, env_val = env_pair_parts[0], '='.join(env_pair_parts[1:])
             env_dict[env_key, env_val]
         return env_dict
 
