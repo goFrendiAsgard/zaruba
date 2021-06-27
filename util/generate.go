4068bb459041d70f6a38761d526bc6b38d6df57f
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-06-27 17:07:12 +0700

    WIP: create generator

---
+++ util/generate.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func Generate(templateLocation, destination string, replacementMap map[string]string) {
+	err := filepath.Walk(templateLocation,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			fmt.Println(path, info.Size())
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+}
