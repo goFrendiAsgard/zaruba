4c0cb670ad6e9069b14d10ba51fc5b3b0ca24b6c
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-07-31 11:13:41 +0700

    remove setup scripts

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
 set +e
 echo "💀 Remove old Zaruba"
-go clean -i github.com/state-alchemists/zaruba
-if [ -f "${HOME}/.zaruba/scripts/bash/bootstrap.sh" ]
-then
-    echo "💀 Backup bootstrap script"
-    cp "${HOME}/.zaruba/scripts/bash/bootstrap.sh" "${HOME}/zaruba-bootstrap.sh.bak"
-fi
 rm -Rf "${HOME}/.zaruba"
 
 set -e
-echo "💀 Cloning Zaruba"
+echo "💀 Clone Zaruba"
 git clone --depth 1 https://github.com/state-alchemists/zaruba "${HOME}/.zaruba"
-if [ -f "${HOME}/zaruba-bootstrap.sh.bak" ]
-then
-    echo "💀 Restore bootstrap script"
-    mv "${HOME}/zaruba-bootstrap.sh.bak" "${HOME}/.zaruba/scripts/bash/bootstrap.sh"
-fi
 
 echo "💀 Build Zaruba"
 cd "${HOME}/.zaruba"
@@ -62,48 +51,3 @@ else
 fi
 
 echo "💀 Installation success"
-
-echo "💀 zaruba can install several packages in case of you are using ubuntu"
-read -p "💀 Do you want to setup ubuntu? (y/N): "  CONFIRMATION
-if [ "${CONFIRMATION}" = "y" ]
-then
-    zaruba please setupUbuntu -i
-    echo "💀 Ubuntu setup success"
-else
-    echo "💀 You can perform this task later by invoking 'zaruba please setupUbuntu -i'"
-fi
-
-echo ""
-echo "💀 pyenv lets you easily switch between multiple versions of Python"
-echo "💀 Some of zaruba's tasks depend on Python"
-read -p "💀 Do you want to setup pyenv? (y/N): "  CONFIRMATION
-if [ "${CONFIRMATION}" = "y" ]
-then
-    zaruba please setupPyenv -i
-    echo "💀 Pyenv setup success"
-else
-    echo "💀 You can perform this task later by invoking 'zaruba please setupPyenv -i'"
-fi
-
-echo ""
-echo "💀 nvm is a version manager for node.js"
-read -p "💀 Do you want to setup nvm? (y/N): "  CONFIRMATION
-if [ "${CONFIRMATION}" = "y" ]
-then
-    zaruba please setupNvm -i
-    echo 💀 Nvm setup success 
-else
-    echo 💀 You can perform this task later by invoking 'zaruba please setupNvm -i' 
-fi
-
-echo ""
-echo "💀 When you setup pyenv/nvm, zaruba will also make bootstrap script on '~/.zaruba/scripts/bootstrap.sh' to be used internally"
-read -p "💀 Do you want to also inject the bootstrap script into your bash/zsh? (y/N): "  CONFIRMATION
-if [ "${CONFIRMATION}" = "y" ]
-then
-    zaruba please injectBootstrap -i
-    echo 💀 Bootstrap injected 
-else
-    echo 💀 You can perform this task later by invoking 'zaruba please injectBootstrap' 
-fi
-
