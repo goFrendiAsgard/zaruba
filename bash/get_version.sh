fa37d5bac06baa740ec1e70f1b1f7b755e8fd74a
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-08-07 14:50:40 +0700

    fix get_version script

--- bash/get_version.sh
+++ bash/get_version.sh
@@ -3,7 +3,7 @@
 get_version() {
     (echo $- | grep -Eq ^.*e.*$) && _OLD_STATE=-e || _OLD_STATE=+e
     set +e
-    _GIT_VERSION=$(git describe --tags)
+    _GIT_VERSION=$(git describe --tags --always 2>/dev/null)
     if [ ! -z "${_GIT_VERSION}" ]
     then
         echo ${_GIT_VERSION}
