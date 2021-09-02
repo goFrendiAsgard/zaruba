bbf7fea45b1c2396915544c46c167ee15027c7f0
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-09-02 18:00:48 +0700

    make airflow runner exactly like the docker compose counterpart

--- templates/task/docker/airflow/zarubaServiceName/configurator.gotmpl.sh
+++ templates/task/docker/airflow/zarubaServiceName/configurator.gotmpl.sh
@@ -1,4 +1,4 @@
-mkdir -p /sources/logs /sources/dags /sources/plugins
-chown -R "${AIRFLOW_UID}:${AIRFLOW_GID}" /sources/{logs,dags,plugins}
+mkdir -p /opt/airflow/logs /opt/airflow/dags /opt/airflow/plugins
+chown -R "${AIRFLOW_UID}:${AIRFLOW_GID}" /opt/airflow/{logs,dags,plugins}
 exec /entrypoint airflow version
 echo "Init zarubaServiceName configurator"
