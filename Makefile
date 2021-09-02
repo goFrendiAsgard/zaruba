65c95d94635eb59df0bcf07351c24a914a23b8f8
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-09-02 09:39:37 +0700

    WIP: airflow

--- Makefile
+++ Makefile
@@ -3,5 +3,4 @@ build:
 test:
 	go build
 	./zaruba please clearLog
-	sudo rm -Rf playground
 	./zaruba please test runPlaygroundDelay=10 makeTaskDocs
--- templates/task/docker/airflow/zarubaServiceName/configurator.gotmpl.sh
+++ templates/task/docker/airflow/zarubaServiceName/configurator.gotmpl.sh
@@ -1,2 +1,4 @@
-
+mkdir -p /sources/logs /sources/dags /sources/plugins
+chown -R "${AIRFLOW_UID}:${AIRFLOW_GID}" /sources/{logs,dags,plugins}
+exec /entrypoint airflow version
 echo "Init zarubaServiceName configurator"
