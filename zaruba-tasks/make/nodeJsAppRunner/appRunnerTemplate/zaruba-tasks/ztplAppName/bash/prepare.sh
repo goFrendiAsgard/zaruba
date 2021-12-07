a99419618790d347a1b30a09e7ffce61f5d428c5
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2021-12-07 21:40:29 +0700

    update nodejs command

--- zaruba-tasks/make/nodeJsAppRunner/appRunnerTemplate/zaruba-tasks/ztplAppName/bash/prepare.sh
+++ zaruba-tasks/make/nodeJsAppRunner/appRunnerTemplate/zaruba-tasks/ztplAppName/bash/prepare.sh
@@ -1,17 +1,21 @@
 if [ "${SHOULD_REMOVE_NODE_MODULES}" = 1 ]
 then
-    npm -Rf node_modules
+    echo "Remove node_modules"
+    rm -Rf node_modules
 fi
 if [ "${SHOULD_CLEAN_NPM_CACHE}" = 1 ]
 then
+    echo "Clean npm cache"
     npm cache clean --force
 fi
 if [ ! -d "node_modules" ]
 then
-    npm install
+    echo "Install npm packages"
+    npm install --include=dev
 fi
 if [ "${SHOULD_REBUILD_NPM}" = 1 ]
 then
+    echo "Rebuild npm packages"
     npm rebuild
 fi
 if [ "${SHOULD_INSTALL_TS}" = 1 ]
@@ -20,6 +24,7 @@ then
     then
         echo "Typescript is already installed"
     else
+        echo "Install typescript"
         npm install -g typescript${TS_VERSION}
     fi
 fi
@@ -27,8 +32,10 @@ if [ "${SHOULD_COMPILE_TS}" = 1 ]
 then
     if [ -f "./node_modules/.bin/tsc" ]
     then
+        echo "Compile using local tsc"
         ./node_modules/.bin/tsc
     else
+        echo "Compile using global tsc"
         tsc
     fi
 fi
