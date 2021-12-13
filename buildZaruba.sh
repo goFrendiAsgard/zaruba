c6842502d1ce2da71d69ce6a332ef7d599988b36
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2021-12-13 21:34:48 +0700

    build

--- buildZaruba.sh
+++ buildZaruba.sh
@@ -1,4 +1,5 @@
 echo "package cmd" > "cmd/version.go"
 echo "var ZarubaVersion = \"$(git describe --tags --always)\"" >> "cmd/version.go"
-
+git add . -A
+git commit -m 'build'
 go build -o zaruba
--- cmd/version.go
+++ cmd/version.go
@@ -1,2 +1,2 @@
 package cmd
-var ZarubaVersion = "v0.9.0-alpha-1-83-g6dc2c802"
+var ZarubaVersion = "v0.9.0-alpha-1-84-g7b4151af"
