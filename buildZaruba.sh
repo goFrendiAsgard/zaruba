9a32d75b5d35ace984e69be45dfe9c867c474294
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2021-12-14 06:09:06 +0700

    update builder script

--- buildZaruba.sh
+++ buildZaruba.sh
@@ -1,5 +1,1 @@
-# echo "package cmd" > "cmd/version.go"
-# echo "var ZarubaVersion = \"$(git describe --tags --always)\"" >> "cmd/version.go"
 go build -ldflags="-X 'github.com/state-alchemists/zaruba/cmd.ZarubaVersion=$(git describe --tags --always)'" -o zaruba
-git add . -A
-git commit -m 'build'
