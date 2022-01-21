4e9a26825a1747a35eaef31281f1a4da31e0475c
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2022-01-21 21:06:57 +0700

    add lunarvim and starship

---
+++ setup/lunarvim.sh
@@ -0,0 +1,3 @@
+set -e
+
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
---
+++ setup/starship.sh
@@ -0,0 +1,4 @@
+set -e
+
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+echo "Please check https://starship.rs/guide/#%F0%9F%9A%80-installation for more information"
