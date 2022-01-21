8e4ab7dbf0b36852c0ba11ececab4f88f3f0fba8
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2022-01-21 16:19:08 +0700

    add win32yank installation script

---
+++ setup/win32yank.sh
@@ -0,0 +1,6 @@
+set -e
+
+curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe /usr/local/bin/
