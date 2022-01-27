f4adee2634f4ddf8d52005ad55b04f54d21a7d9c
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2022-01-27 07:53:25 +0700

    better script

--- zaruba-tasks/make/appRunner/_base/bash/registerAppDependencies.sh
+++ zaruba-tasks/make/appRunner/_base/bash/registerAppDependencies.sh
@@ -5,21 +5,26 @@ _APP_DEPENDENCIES="${3}"
 
 _PASCAL_APP_NAME="$("${ZARUBA_BIN}" str toPascal "${_APP_NAME}")" 
 
-echo "Registering start${PASCAL_APP_NAME}Container dependencies"
-_CONTAINER_TASK_FILE_NAME="zaruba=tasks/${_APP_NAME}/tasks.container.yaml"
+echo "PROJECT_FILE_NAME ${_PROJECT_FILE_NAME}"
+echo "APP_NAME ${_APP_NAME}"
+echo "APP_DEPENDENCIES ${_APP_DEPENDENCIES}"
+echo "PASCAL_APP_NAME ${_PASCAL_APP_NAME}"
+
+echo "Registering start${_PASCAL_APP_NAME}Container dependencies"
+_CONTAINER_TASK_FILE_NAME="zaruba-tasks/${_APP_NAME}/tasks.container.yaml"
 for _APP_DEPENDENCY_INDEX in $("${ZARUBA_BIN}" list rangeIndex "${_APP_DEPENDENCIES}")
 do
-    _APP_DEPENDENCCY="$("${ZARUBA_BIN}" list get "${_APP_DEPENDENCIES}" "${_APP_DEPENDENCY_INDEX}")"
+    _APP_DEPENDENCY_NAME="$("${ZARUBA_BIN}" list get "${_APP_DEPENDENCIES}" "${_APP_DEPENDENCY_INDEX}")"
     _PASCAL_APP_DEPENDENCY_NAME="$("${ZARUBA_BIN}" str toPascal "${_APP_DEPENDENCY_NAME}")" 
     _addTaskDependency "${_CONTAINER_TASK_FILE_NAME}" "start${_PASCAL_APP_NAME}Container" "start${_PASCAL_APP_DEPENDENCY_NAME}Container" 0 "${_PROJECT_FILE_NAME}"
 done
 
 
-echo "Registering start${PASCAL_APP_NAME} dependencies"
-_TASK_FILE_NAME="zaruba=tasks/${_APP_NAME}/tasks.yaml"
+echo "Registering start${_PASCAL_APP_NAME} dependencies"
+_TASK_FILE_NAME="zaruba-tasks/${_APP_NAME}/tasks.yaml"
 for _APP_DEPENDENCY_INDEX in $("${ZARUBA_BIN}" list rangeIndex "${_APP_DEPENDENCIES}")
 do
-    _APP_DEPENDENCCY="$("${ZARUBA_BIN}" list get "${_APP_DEPENDENCIES}" "${_APP_DEPENDENCY_INDEX}")"
+    _APP_DEPENDENCY_NAME="$("${ZARUBA_BIN}" list get "${_APP_DEPENDENCIES}" "${_APP_DEPENDENCY_INDEX}")"
     _PASCAL_APP_DEPENDENCY_NAME="$("${ZARUBA_BIN}" str toPascal "${_APP_DEPENDENCY_NAME}")" 
     _addTaskDependency "${_TASK_FILE_NAME}" "start${_PASCAL_APP_NAME}Container" "start${_PASCAL_APP_DEPENDENCY_NAME}Container" 0 "${_PROJECT_FILE_NAME}"
 done
