bd0a07f0380409e184bed7a58d4e8126bdbdb044
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2022-02-18 06:41:55 +0700

    add master spark url example

--- zaruba-tasks/make/spark/appTemplate/ztplAppDirectory/example/spark-submit-docker.sh
+++ zaruba-tasks/make/spark/appTemplate/ztplAppDirectory/example/spark-submit-docker.sh
@@ -2,4 +2,10 @@ echo "Copy example.py to spark master container"
 docker cp example.py ztplAppContainerNameSparkMaster:/opt/bitnami/example.py
 
 echo "Perform spark submit"
-docker exec -it ztplAppContainerNameSparkMaster spark-submit /opt/bitnami/example.py
+docker exec -it ztplAppContainerNameSparkMaster \
+    spark-submit \
+    --master spark://localhost:7077 \
+    /opt/bitnami/example.py
+
+echo "Remove example.py from the container"
+docker exec -it ztplAppContainerNameSparkMaster rm -Rf /opt/bitnami/example.py
