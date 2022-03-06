adcf27c3ec0097d02bc4e7fff7f9906d92acea90
gofrendi <gofrendiasgard@gmail.com>
2022-03-06 08:02:23 +0700

    better tag

--- buildZaruba.sh
+++ buildZaruba.sh
@@ -1,1 +1,3 @@
-go build -ldflags="-X 'github.com/state-alchemists/zaruba/cmd.ZarubaVersion=$(git describe --tags --always)'" -o zaruba
+TAG=$(git describe $(git rev-list --tags --max-count=1))
+BUILD=$(git rev-parse HEAD)
+go build -ldflags="-X 'github.com/state-alchemists/zaruba/cmd.ZarubaVersion=${TAG}-${BUILD}'" -o zaruba
