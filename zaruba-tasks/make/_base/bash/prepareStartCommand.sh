e91e96ddf5b8fc0eacf1fdc10f5d3e59931ee8e4
gofrendi <gofrendiasgard@gmail.com>
2022-05-16 07:23:18 +0700

    revert false positive changes

--- zaruba-tasks/make/_base/bash/prepareStartCommand.sh
+++ zaruba-tasks/make/_base/bash/prepareStartCommand.sh
@@ -2,30 +2,30 @@ set -e
 echo "Preparing start command"
 
 # start command
-if [ -z "${_ZRB_APP_START_ICON_COMMAND}" ]
+if [ -z "${_ZRB_APP_START_COMMAND}" ]
 then
     if [ -f "${_ZRB_APP_DIRECTORY}/start.sh" ]
     then
-        _ZRB_APP_START_ICON_COMMAND="./start.sh"
+        _ZRB_APP_START_COMMAND="./start.sh"
     elif [ -f "${_ZRB_APP_DIRECTORY}/main.go" ]
     then
-        _ZRB_APP_START_ICON_COMMAND="go run main.go"
+        _ZRB_APP_START_COMMAND="go run main.go"
     elif [ -f "${_ZRB_APP_DIRECTORY}/package.json" ]
     then
-        _ZRB_APP_START_ICON_COMMAND="npm start"
+        _ZRB_APP_START_COMMAND="npm start"
     elif [ -f "${_ZRB_APP_DIRECTORY}/main.py" ]
     then
         if [ -f "${_ZRB_APP_DIRECTORY}/Pipfile" ]
         then
-            _ZRB_APP_START_ICON_COMMAND="pipenv run python main.py"
+            _ZRB_APP_START_COMMAND="pipenv run python main.py"
         elif [ -f "${_ZRB_APP_DIRECTORY}/venv/bin/activate" ]
         then
-            _ZRB_APP_START_ICON_COMMAND="source ./venv/bin/activate && python main.py"
+            _ZRB_APP_START_COMMAND="source ./venv/bin/activate && python main.py"
         else
-            _ZRB_APP_START_ICON_COMMAND="python main.py"
+            _ZRB_APP_START_COMMAND="python main.py"
         fi
     else
-        _ZRB_APP_START_ICON_COMMAND="echo \"Replace this with command to start ${_ZRB_APP_NAME}\" && exit 1"
+        _ZRB_APP_START_COMMAND="echo \"Replace this with command to start ${_ZRB_APP_NAME}\" && exit 1"
     fi
 fi
 
--- zaruba-tasks/make/nodeJsAppRunner/bash/prepareVariables.sh
+++ zaruba-tasks/make/nodeJsAppRunner/bash/prepareVariables.sh
@@ -3,7 +3,7 @@ then
     _ZRB_APP_TEST_COMMAND='npm test'
 fi
 
-if [ -z "${_ZRB_APP_START_ICON_COMMAND}" ]
+if [ -z "${_ZRB_APP_START_COMMAND}" ]
 then
-    _ZRB_APP_START_ICON_COMMAND="npm start"
+    _ZRB_APP_START_COMMAND="npm start"
 fi
