d4896c754be621be12baf55510d63f673410780f
gofrendi <gofrendiasgard@gmail.com>
2022-06-13 23:25:21 +0700

    add auth service requirement

--- zaruba-tasks/make/fastApi/appTemplate/ztplAppDirectory/ui/createMenuService.py
+++ zaruba-tasks/make/fastApi/appTemplate/ztplAppDirectory/ui/createMenuService.py
@@ -1,8 +1,9 @@
 from ui.menuService import MenuService, DefaultMenuService
+from auth.authService import AuthService
 from auth.roleService import RoleService
 
-def create_menu_service(role_service: RoleService) -> MenuService:
-    menu_service = DefaultMenuService(role_service)
+def create_menu_service(auth_service: AuthService, role_service: RoleService) -> MenuService:
+    menu_service = DefaultMenuService(auth_service, role_service)
     menu_service.add_menu(name='auth', title='Auth', url='/auth')
     menu_service.add_menu(name='auth/roles', title='Roles', url='/auth/roles', parent_name='auth')
     menu_service.add_menu(name='auth/users', title='Users', url='/auth/users', parent_name='auth')
