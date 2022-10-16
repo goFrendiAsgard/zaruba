e95f67707da76179babe0491ba795a4450b5662e
gofrendi <gofrendiasgard@gmail.com>
2022-10-16 19:44:03 +0700

    add snorlax for custom error

--- zaruba-tasks/make/fastApp/appTemplate/ztplAppDirectory/configs/pageTemplateFactory.py
+++ zaruba-tasks/make/fastApp/appTemplate/ztplAppDirectory/configs/pageTemplateFactory.py
@@ -1,7 +1,7 @@
 from fastapi.templating import Jinja2Templates
 from configs.dir import page_dir
 from configs.auth import guest_username
-from configs.ui import site_name, renew_access_token_interval
+from configs.ui import site_name, tagline, footer, renew_access_token_interval
 from configs.url import backend_url, public_url_path, renew_access_token_url_path
 
 import os
@@ -13,6 +13,8 @@ def create_page_template() -> Jinja2Templates:
     templates = Jinja2Templates(directory=page_dir)
     templates.env.globals['guest_username'] = guest_username
     templates.env.globals['site_name'] = site_name
+    templates.env.globals['tagline'] = tagline
+    templates.env.globals['footer'] = footer
     templates.env.globals['backend_url'] = backend_url
     templates.env.globals['public_url_path'] = public_url_path
     templates.env.globals['renew_access_token_url_path'] = renew_access_token_url_path
