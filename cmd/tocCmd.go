130c4b23724496d2e05475e5c56a079349678193
goFrendi <gofrendiasgard@gmail.com>
2023-01-13 08:45:54 +0700

    improve toc documentation

--- cmd/tocCmd.go
+++ cmd/tocCmd.go
@@ -7,6 +7,25 @@ import (
 	"github.com/state-alchemists/zaruba/toc"
 )
 
+var tocLong = `
+Create/update documentation directory based on a TOC file.
+
+A TOC file is markdown file representing "table of content" for your documentation.
+A tag is written as <!--start[TagName] attribute="value" -->content<!--end[TagName]-->
+
+There are several tagname available for a TOC file:
+    - toc: This tag contains list of bulleted-items that will be rendered into documentation structure.
+    - code: This tag has several attributes:
+        - lang: code language (e.g., python, javascript, bash)
+        - src: location of your source code file, relative to TOC file directory
+        - cmd: Command to run your code.
+      Your code tag will be rendered into a markdown.    
+
+There are also additional tagName for your documentation files:
+    - tocHeader: This tag will be filled with documentation header
+    - tocSubtopic: This tag will be filled with documentation subtopics
+`
+
 var tocExample = `
 > cat README.md
 # My Cool Project
@@ -26,7 +45,8 @@ This is a documentation for my cool project.
 
 var tocCmd = &cobra.Command{
 	Use:     "toc <tocFileLocation>",
-	Short:   "Create/update documentations based on TOC file",
+	Short:   "Create/update documentation directory based on a TOC file",
+	Long:    tocLong,
 	Example: tocExample,
 	Run: func(cmd *cobra.Command, args []string) {
 		decoration := output.NewDefaultDecoration()
--- docs/built-in/utils/toc.md
+++ docs/built-in/utils/toc.md
@@ -4,7 +4,23 @@
 <!--endTocHeader-->
 
 ```
-Create/update documentations based on TOC file
+
+Create/update documentation directory based on a TOC file.
+
+A TOC file is markdown file representing "table of content" for your documentation.
+A tag is written as <!--start[TagName] attribute="value" -->content<!--end[TagName]-->
+
+There are several tagname available for a TOC file:
+    - toc: This tag contains list of bulleted-items that will be rendered into documentation structure.
+    - code: This tag has several attributes:
+        - lang: code language (e.g., python, javascript, bash)
+        - src: location of your source code file, relative to TOC file directory
+        - cmd: Command to run your code.
+      Your code tag will be rendered into a markdown.    
+
+There are also additional tagName for your documentation files:
+    - tocHeader: This tag will be filled with documentation header
+    - tocSubtopic: This tag will be filled with documentation subtopics
 
 Usage:
   zaruba toc <tocFileLocation> [flags]
